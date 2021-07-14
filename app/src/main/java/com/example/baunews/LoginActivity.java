d345e6c2745ad9fed214e2fde099d2609c0ce18c
shawqimorad <48058171+shawqimorad@users.noreply.github.com>
2021-07-14 16:07:19 +0300

    register added...

--- app/src/main/java/com/example/baunews/LoginActivity.java
+++ app/src/main/java/com/example/baunews/LoginActivity.java
@@ -2,16 +2,30 @@ package com.example.baunews;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.view.WindowManager;
+import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
 
+    TextView register ;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         getWindow().setExitTransition(null);
         getWindow().setEnterTransition(null);
+
+        //note : use bining view later....
+        register = findViewById(R.id.sign_up);
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginActivity.this,RegisterActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/baunews/RegisterActivity.java
+++ app/src/main/java/com/example/baunews/RegisterActivity.java
@@ -2,13 +2,28 @@ package com.example.baunews;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class RegisterActivity extends AppCompatActivity {
+
+    TextView login;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        //note : connect string array to dropdown when use bining view.
+         login = findViewById(R.id.sign_in);
+         login.setOnClickListener(new View.OnClickListener() {
+             @Override
+             public void onClick(View v) {
+                 Intent intent = new Intent(RegisterActivity.this,LoginActivity.class);
+                 startActivity(intent);
+             }
+         });
     }
 }
