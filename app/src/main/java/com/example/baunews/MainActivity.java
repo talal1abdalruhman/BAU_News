ccbf7e663b05c3e64e567b142575219859195551
shawqimorad <48058171+shawqimorad@users.noreply.github.com>
2021-07-18 02:05:42 +0300

    edit on MainActivity

--- app/src/main/java/com/example/baunews/MainActivity.java
+++ app/src/main/java/com/example/baunews/MainActivity.java
@@ -1,20 +1,16 @@
 package com.example.baunews;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.view.GravityCompat;
 import androidx.databinding.DataBindingUtil;
-
 import android.content.Intent;
-import android.graphics.Color;
-import android.graphics.ColorSpace;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
-import android.view.WindowManager;
 
-import com.example.baunews.databinding.ActivityLoginBinding;
 import com.example.baunews.databinding.ActivityMainBinding;
 import com.google.android.material.navigation.NavigationView;
 
@@ -27,14 +23,18 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         super.onCreate(savedInstanceState);
         binding = DataBindingUtil.setContentView(this,R.layout.activity_main);
 
-        binding.menu.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                binding.drawer.openDrawer(GravityCompat.START);
-            }
-        });
-        binding.nav.setItemIconTintList(null);
+        setSupportActionBar(binding.toolbar);
         binding.nav.setNavigationItemSelectedListener(this);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, binding.drawerLayout, binding.toolbar,
+                R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        binding.drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+        if(savedInstanceState == null){
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.fragment_container,new GeneralFragment()).commit();
+            binding.nav.setCheckedItem(R.id.general);
+        }
+
         binding.floatingActionButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -43,24 +43,12 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
             }
         });
 
-        if(savedInstanceState == null){
-            getSupportFragmentManager().beginTransaction()
-                    .replace(R.id.load,new GeneralFragment()).commit();
-            binding.nav.setCheckedItem(R.id.general);
-        }
-        binding.floatingActionButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent=new Intent(MainActivity.this,CreateNewsActivity.class);
-                startActivity(intent);
-            }
-        });
     }
 
     @Override
     public void onBackPressed() {
-        if(binding.drawer.isDrawerOpen(GravityCompat.START)){
-            binding.drawer.closeDrawer(GravityCompat.START);
+        if(binding.drawerLayout.isDrawerOpen(GravityCompat.START)){
+            binding.drawerLayout.closeDrawer(GravityCompat.START);
         }
         else
             super.onBackPressed();
@@ -71,14 +59,14 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         if(item.getItemId() == R.id.general){
             Log.d("General","Clicked");
             getSupportFragmentManager().beginTransaction()
-                    .replace(R.id.load,new GeneralFragment()).commit();
+                    .replace(R.id.fragment_container,new GeneralFragment()).commit();
         }
         if(item.getItemId() == R.id.college){
             Log.d("College","Clicked");
             getSupportFragmentManager().beginTransaction()
-                    .replace(R.id.load,new CollegeFragment()).commit();
+                    .replace(R.id.fragment_container,new CollegeFragment()).commit();
         }
-        binding.drawer.closeDrawer(GravityCompat.START);
+        binding.drawerLayout.closeDrawer(GravityCompat.START);
         return true;
     }
 }
