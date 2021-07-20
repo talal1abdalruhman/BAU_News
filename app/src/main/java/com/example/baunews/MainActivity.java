50c9c33002e8d42c3f5094e9fe6b051d67fab127
shawqimorad <48058171+shawqimorad@users.noreply.github.com>
2021-07-20 16:00:12 +0300

    Navigation Finished....

--- app/src/main/java/com/example/baunews/MainActivity.java
+++ app/src/main/java/com/example/baunews/MainActivity.java
@@ -1,21 +1,19 @@
 package com.example.baunews;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.view.GravityCompat;
 import androidx.databinding.DataBindingUtil;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.NavigationUI;
+
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.MenuItem;
 import android.view.View;
-import android.widget.Toast;
 
 import com.example.baunews.databinding.ActivityMainBinding;
-import com.google.android.material.navigation.NavigationView;
 
-public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+public class MainActivity extends AppCompatActivity {
 
     ActivityMainBinding binding;
 
@@ -32,44 +30,14 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
             }
         });
 
-        setSupportActionBar(binding.toolbar);
-        binding.nav.setNavigationItemSelectedListener(this);
-        binding.nav.bringToFront();
-        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, binding.drawerLayout, binding.toolbar,
-                R.string.navigation_drawer_open, R.string.navigation_drawer_close);
-        binding.drawerLayout.addDrawerListener(toggle);
-        toggle.syncState();
-        if(savedInstanceState == null){
-            getSupportFragmentManager().beginTransaction()
-                    .add(R.id.fragment_container,new CollegeFragment()).commit();
-            binding.nav.setCheckedItem(R.id.college);
-        }
-    }
-
-    @Override
-    public void onBackPressed() {
-        if(binding.drawerLayout.isDrawerOpen(GravityCompat.START)){
-            binding.drawerLayout.closeDrawer(GravityCompat.START);
-        }
-        else
-            super.onBackPressed();
-    }
-
-    @Override
-    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-        if(item.getItemId() == R.id.general){
-            Toast.makeText(this,"General",Toast.LENGTH_SHORT).show();
-            getSupportFragmentManager().beginTransaction()
-                    .replace(R.id.fragment_container,new GeneralFragment()).commit();
-            binding.nav.setCheckedItem(R.id.general);
-        }
-        if(item.getItemId() == R.id.college){
-            Toast.makeText(this,"College",Toast.LENGTH_SHORT).show();
-            getSupportFragmentManager().beginTransaction()
-                    .replace(R.id.fragment_container,new CollegeFragment()).commit();
-            binding.nav.setCheckedItem(R.id.college);
-        }
-        binding.drawerLayout.closeDrawer(GravityCompat.START);
-        return true;
+        binding.menu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                binding.drawerLayout.openDrawer(GravityCompat.START);
+            }
+        });
+        binding.navigationView.setItemIconTintList(null);
+        NavController navController = Navigation.findNavController(this,R.id.fragment);
+        NavigationUI.setupWithNavController(binding.navigationView,navController);
     }
 }
