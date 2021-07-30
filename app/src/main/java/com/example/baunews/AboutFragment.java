2752a41c9644c9acea3a91dc685aa2839d6e33b6
huthifa.almnasrah@yahoo.com <huthifa.almnasrah@yahoo.com>
2021-07-31 02:46:58 +0300

    Java code to about fragment

--- app/src/main/java/com/example/baunews/AboutFragment.java
+++ app/src/main/java/com/example/baunews/AboutFragment.java
@@ -2,18 +2,25 @@ package com.example.baunews;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.databinding.DataBindingUtil;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.example.baunews.databinding.FragmentAboutBinding;
+
 /**
  * A simple {@link Fragment} subclass.
  * Use the {@link AboutFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class AboutFragment extends Fragment {
+public class AboutFragment extends Fragment implements View.OnClickListener {
+
+    private FragmentAboutBinding binding;
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -53,16 +60,111 @@ public class AboutFragment extends Fragment {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
 
-
-
-
         }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_about, container, false);
+        View view=binding.getRoot();
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_about, container, false);
+        return view;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        binding.aboutBau.setOnClickListener(this);
+        binding.royalLetter.setOnClickListener(this);
+        binding.prepresidentLetter.setOnClickListener(this);
+        binding.vision.setOnClickListener(this);
+        binding.aboutColleges.setOnClickListener(this);
+    }
+
+
+    public void rotateUp(View v){
+        v.setPivotX(v.getWidth()/2);
+        v.setPivotY(v.getHeight()/2);
+        v.setRotation(0);
+        v.animate().setDuration(400).rotation(90);
+        v.animate().setDuration(400).rotation(180);
+    }
+    public void rotateDown(View v){
+        v.setPivotX(v.getWidth()/2);
+        v.setPivotY(v.getHeight()/2);
+        v.animate().setDuration(400).rotation(270);
+        v.animate().setDuration(400).rotation(360);
+    }
+
+    final int []heights={1699,3861,2075,1323,3080};
+
+    final boolean[] isclick = {false,false,false,false,false};
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.about_bau:
+                if(isclick[0]==false){
+                    rotateUp(binding.aboutBauArrow);
+                    binding.aboutBauTxt.setVisibility(View.VISIBLE);
+                    binding.aboutBauTxt.setHeight(heights[0]);
+                }
+                else{
+                    rotateDown(binding.aboutBauArrow);
+                    binding.aboutBauTxt.setVisibility(View.INVISIBLE);
+                    binding.aboutBauTxt.setHeight(0);
+                }
+                isclick[0] =!isclick[0];
+                break;
+            case  R.id.royal_letter:
+                if(isclick[1]==false){
+                    rotateUp(binding.royalArrow);
+                    binding.txtRoyalLetter.setVisibility(View.VISIBLE);
+                    binding.txtRoyalLetter.setHeight(heights[1]);
+                }
+                else{
+                    rotateDown(binding.royalArrow);
+                    binding.txtRoyalLetter.setVisibility(View.INVISIBLE);
+                    binding.txtRoyalLetter.setHeight(0);
+                }
+                isclick[1] =!isclick[1];
+                break;
+            case R.id.prepresident_letter:
+                if(isclick[2]==false){
+                    rotateUp(binding.prepresidentArrow);
+                    binding.txtPrepresidentLetter.setVisibility(View.VISIBLE);
+                    binding.txtPrepresidentLetter.setHeight(heights[2]);
+                }
+                else{
+                    rotateDown(binding.prepresidentArrow);
+                    binding.txtPrepresidentLetter.setVisibility(View.INVISIBLE);
+                    binding.txtPrepresidentLetter.setHeight(0);
+                }
+                isclick[2] =!isclick[2];
+                break;
+            case R.id.vision:
+                if(isclick[3]==false){
+                    rotateUp(binding.visionArrow);
+                    binding.txtVision.setVisibility(View.VISIBLE);
+                    binding.txtVision.setHeight(heights[3]);
+                }
+                else{
+                    rotateDown(binding.visionArrow);
+                    binding.txtVision.setVisibility(View.INVISIBLE);
+                    binding.txtVision.setHeight(0);
+                }
+                isclick[3] =!isclick[3];
+                break;
+            case R.id.about_colleges:
+                if(isclick[4]==false){
+                    rotateUp(binding.collegeArrow);
+                    binding.scroll.setVisibility(View.VISIBLE);
+                }
+                else{
+                    rotateDown(binding.collegeArrow);
+                    binding.scroll.setVisibility(View.INVISIBLE);
+                }
+                isclick[4] =!isclick[4];
+        }
     }
 }
