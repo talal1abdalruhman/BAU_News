f0260156f8139ff93bc6b6534aa438a85b600fa9
huthifa.almnasrah@yahoo.com <huthifa.almnasrah@yahoo.com>
2021-08-05 16:33:16 +0300

    done

--- app/src/main/java/com/example/baunews/CollegeFragment.java
+++ app/src/main/java/com/example/baunews/CollegeFragment.java
@@ -21,6 +21,9 @@ import android.util.TypedValue;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Toast;
 
 
 import com.example.baunews.HelperClasses.NewsAdapter;
@@ -136,4 +139,5 @@ public class CollegeFragment extends Fragment {
                     }
                 });
     }
+
 }
