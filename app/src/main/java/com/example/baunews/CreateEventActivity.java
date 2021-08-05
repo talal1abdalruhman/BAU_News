d4bcfd31a634bbbfcb43dc908c5b4b1eef70fb52
huthifa.almnasrah@yahoo.com <huthifa.almnasrah@yahoo.com>
2021-08-05 10:16:25 +0300

    coding Create Event Activity

--- app/src/main/java/com/example/baunews/CreateEventActivity.java
+++ app/src/main/java/com/example/baunews/CreateEventActivity.java
@@ -1,14 +1,314 @@
 package com.example.baunews;
 
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.databinding.DataBindingUtil;
 
+import android.Manifest;
+import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.graphics.drawable.ColorDrawable;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.provider.OpenableColumns;
+import android.text.format.DateFormat;
+import android.util.Log;
+import android.util.Patterns;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TimePicker;
+import android.widget.Toast;
+
+import com.example.baunews.databinding.ActivityCreateEventBinding;
+import com.google.android.material.bottomsheet.BottomSheetBehavior;
+
+import java.io.File;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Locale;
 
 public class CreateEventActivity extends AppCompatActivity {
+
+    ActivityCreateEventBinding binding;
+
+    private static final int IMAGE_REQUEST_CODE = 100;
+    private static final int FILE_REQUEST_CODE = 101;
+
+    Uri ImgUri,PdfUri;
+
+    int hour,minute,year,month,day;
+    String date,time,altrTime,altrDate,startEventDateAndTime;
+
+    private AlertDialog dialogAddURL;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_event);
+
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_create_event);
+
+        binding.btnDate.setText(new SimpleDateFormat("dd/M/yyyy", Locale.getDefault()).format(new Date()));
+        binding.btnTime.setText(new SimpleDateFormat("HH:mm a", Locale.getDefault()).format(new Date()));
+        binding.txtDateAndTime.setText(new SimpleDateFormat("EEEE, dd MMMM yyyy HH:mm a", Locale.getDefault()).format(new Date()));
+        AddOthers();
+
+        //-----------------------------------------------------------------------Event Date--------
+        binding.btnDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showDatePicker();
+            }
+        });
+        //-----------------------------------------------------------------------Event Time--------
+        binding.btnTime.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showTimePicker();
+            }
+        });
+        //--------------------------------------------------------------------save button-----------------
+        binding.btnSave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                //------------------------------------------If the admin does not choose the date or time---------------------------
+
+                altrTime=new SimpleDateFormat(" HH:mm a", Locale.getDefault()).format(new Date());
+                altrDate=new SimpleDateFormat("EEEE, dd MMMM yyyy", Locale.getDefault()).format(new Date());
+                if(time == null){
+                    time=altrTime;
+                }
+                if (date == null){
+                    date=altrDate;
+                }
+                if(time != null || date != null){
+                    startEventDateAndTime=date+time;
+                }
+                Toast.makeText(CreateEventActivity.this,startEventDateAndTime,Toast.LENGTH_LONG).show();
+            }
+        });
+
+
+        binding.removeImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                binding.imageNews.setImageURI(null);
+                binding.imageNews.setVisibility(View.GONE);
+                binding.removeImage.setVisibility(View.GONE);
+            }
+        });
+        binding.removeWebURL.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                binding.textWebURL.setText(null);
+                binding.layoutWebURL.setVisibility(View.GONE);
+            }
+        });
+        binding.removePdf.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                binding.textPdf.setText(null);
+                binding.layoutPdf.setVisibility(View.GONE);
+            }
+        });
+        //--------------------------------------------------------------------back button----------
+
+        binding.btnBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(CreateEventActivity.this,MainActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+    //---------------------------------------------------------------------------------------------
+    //-----------------------------------------------------------------------------TimePicker------
+    private void showTimePicker() {
+        TimePickerDialog.OnTimeSetListener listener=new TimePickerDialog.OnTimeSetListener() {
+            @Override
+            public void onTimeSet(TimePicker timePicker, int hourS, int minuteS) {
+                hour=hourS;
+                minute=minuteS;
+                Calendar calendar=Calendar.getInstance();
+                calendar.set(0,0,0,hour,minute);
+                time = DateFormat.format(" hh:mm aa", calendar) + "";
+                binding.btnTime.setText(time);
+            }
+        };
+        TimePickerDialog timeDialog =new TimePickerDialog(CreateEventActivity.this,listener,hour,minute,false);
+        timeDialog.show();
+    }
+
+    //-----------------------------------------------------------------------------DatePicker------
+    private void showDatePicker() {
+        DatePickerDialog.OnDateSetListener listener =new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker datePicker, int yearS, int monthS, int dayS) {
+                monthS+=1;
+                year=yearS;
+                month=monthS;
+                day=dayS;
+                Calendar calendar=Calendar.getInstance();
+                calendar.set(year,month,day);
+                date = DateFormat.format("EEEE, dd MMMM yyyy", calendar) + "";
+                binding.btnDate.setText(day+"/"+month+"/"+year);
+            }
+        };
+        DatePickerDialog dateDialog=new DatePickerDialog(CreateEventActivity.this,listener,year,month,day);
+        //dateDialog.getWindow().setBackgroundDrawableResource(R.color.mainColor);
+        dateDialog.show();
+    }
+
+
+
+
+    //-------------------------------------------------------------------------------Others--------
+    private void AddOthers() {
+        final LinearLayout layout = findViewById(R.id.layoutAddOthers);
+        Log.d("OthersId", "AddOthers: " + layout.getId());
+        final BottomSheetBehavior<LinearLayout> bottomSheetBehavior = BottomSheetBehavior.from(layout);
+        layout.findViewById(R.id.others).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (bottomSheetBehavior.getState() != BottomSheetBehavior.STATE_EXPANDED) {
+                    bottomSheetBehavior.setState(BottomSheetBehavior.STATE_EXPANDED);
+                } else {
+                    bottomSheetBehavior.setState(BottomSheetBehavior.STATE_COLLAPSED);
+                }
+            }
+        });
+
+        //--------------------------------------------------------------------add Image------------
+        layout.findViewById(R.id.addImage).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                bottomSheetBehavior.setState(BottomSheetBehavior.STATE_COLLAPSED);
+                if (ContextCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+                    ActivityCompat.requestPermissions(CreateEventActivity.this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, IMAGE_REQUEST_CODE);
+                } else {
+                    Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.INTERNAL_CONTENT_URI);
+                    startActivityForResult(intent, IMAGE_REQUEST_CODE);
+                }
+            }
+        });
+        //--------------------------------------------------------------------add Url--------------
+        layout.findViewById(R.id.addUrl).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                bottomSheetBehavior.setState(BottomSheetBehavior.STATE_COLLAPSED);
+                showAddURLDialog();
+            }
+        });
+        //--------------------------------------------------------------------add Pdf--------------
+        layout.findViewById(R.id.addPdf).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                bottomSheetBehavior.setState(BottomSheetBehavior.STATE_COLLAPSED);
+                if (ContextCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+                    ActivityCompat.requestPermissions(CreateEventActivity.this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, FILE_REQUEST_CODE);
+                } else {
+                    Intent intent = new Intent();
+                    intent.setType("application/pdf");
+                    intent.setAction(Intent.ACTION_GET_CONTENT);
+                    startActivityForResult(Intent.createChooser(intent, "Select file"), FILE_REQUEST_CODE);
+                }
+            }
+        });
+    }
+    //---------------------------------------------------------------------------------------------
+    //--------------------------------------------------------------------URL Dialog---------------
+    private void showAddURLDialog() {
+        if (dialogAddURL == null) {
+            AlertDialog.Builder builder = new AlertDialog.Builder(CreateEventActivity.this);
+            View view = LayoutInflater.from(this).inflate(
+                    R.layout.layout_add_url, findViewById(R.id.layoutAddUrlContainer)
+            );
+            builder.setView(view);
+            dialogAddURL = builder.create();
+
+            if (dialogAddURL.getWindow() != null) {
+                dialogAddURL.getWindow().setBackgroundDrawable(new ColorDrawable(0));
+            }
+            final EditText inputURL = view.findViewById(R.id.inputUrl);
+            inputURL.requestFocus();
+
+            view.findViewById(R.id.textAdd).setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    if (inputURL.getText().toString().trim().isEmpty()) {
+                        Toast.makeText(CreateEventActivity.this, "Enter URL", Toast.LENGTH_SHORT).show();
+                    } else if (!Patterns.WEB_URL.matcher(inputURL.getText().toString()).matches()) {
+                        Toast.makeText(CreateEventActivity.this, "Enter Valid URL", Toast.LENGTH_SHORT).show();
+                    } else {
+                        binding.textWebURL.setText(inputURL.getText().toString());
+                        binding.layoutWebURL.setVisibility(View.VISIBLE);
+                        dialogAddURL.dismiss();
+                    }
+                }
+            });
+
+            view.findViewById(R.id.textCancel).setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    dialogAddURL.dismiss();
+                }
+            });
+        }
+
+        dialogAddURL.show();
+    }
+    //--------------------------------------------------------------------result ------------------
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == IMAGE_REQUEST_CODE && resultCode == RESULT_OK) {
+            ImgUri = null;
+            if (data != null) {
+                ImgUri = data.getData();
+            }
+            binding.imageNews.setImageURI(ImgUri);
+            binding.imageNews.setVisibility(View.VISIBLE);
+            binding.removeImage.setVisibility(View.VISIBLE);
+        }
+        if (requestCode == FILE_REQUEST_CODE && resultCode == RESULT_OK) {
+            PdfUri = null;
+            String pdfFileName = null;
+            if (data != null) {
+                PdfUri = data.getData();
+                String uriString = PdfUri.toString();
+                File myFile = new File(uriString);
+
+                if (uriString.startsWith("content://")) {
+                    Cursor cursor = null;
+                    try {
+                        cursor = getContentResolver().query(PdfUri, null, null, null, null);
+                        if (cursor != null && cursor.moveToFirst()) {
+                            pdfFileName = cursor.getString(cursor.getColumnIndex(OpenableColumns.DISPLAY_NAME));
+                        }
+                    } finally {
+                        cursor.close();
+                    }
+                } else if (uriString.startsWith("file://")) {
+                    pdfFileName = myFile.getName();
+                }
+            }
+
+            String ss = PdfUri.getPath();
+            binding.textPdf.setText(pdfFileName);
+            binding.layoutPdf.setVisibility(View.VISIBLE);
+            binding.removePdf.setVisibility(View.VISIBLE);
+        }
     }
 }
