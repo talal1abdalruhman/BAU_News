4e305698ee987a9dd3645c71a5e05c7150970658
Talal Albalawneh <talal1abdalruhman@users.noreply.github.com>
2021-08-19 17:19:47 +0300

    event notification one day, one hour and at time finished

--- app/src/main/java/com/example/baunews/HelperClasses/EventsAdapter.java
+++ app/src/main/java/com/example/baunews/HelperClasses/EventsAdapter.java
@@ -2,18 +2,21 @@ package com.example.baunews.HelperClasses;
 
 import android.annotation.SuppressLint;
 import android.app.AlarmManager;
+import android.app.Notification;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.res.Resources;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.media.RingtoneManager;
 import android.net.Uri;
 import android.os.Build;
 import android.os.CountDownTimer;
+import android.os.SystemClock;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -49,8 +52,13 @@ import static android.content.Intent.FLAG_INCLUDE_STOPPED_PACKAGES;
 
 public class EventsAdapter extends RecyclerView.Adapter<EventsAdapter.MyViewHolder> {
     private static final String TAG = "DATE_TIME";
+    public static final String NOTIFICATION_CHANNEL_ID = "10001";
+    private final static String default_notification_channel_id = "default";
+    private final long HOUR_MILLIS = 3600000;
+    private final long DAY_MILLIS = 86400000;
     private Context context;
     private ArrayList<EventsModel> eventsModelList;
+    SharedPreferences lang, eventNS;
 
     public class MyViewHolder extends RecyclerView.ViewHolder {
         public TextView title, date, startDate;
@@ -69,6 +77,8 @@ public class EventsAdapter extends RecyclerView.Adapter<EventsAdapter.MyViewHold
     public EventsAdapter(Context context, ArrayList<EventsModel> eventsModelList) {
         this.context = context;
         this.eventsModelList = eventsModelList;
+        this.lang = context.getSharedPreferences("LANGUAGE_PREFERENCE", Context.MODE_PRIVATE);
+        this.eventNS = context.getSharedPreferences("NOTIFICATION_SCHEDULE", Context.MODE_PRIVATE);
     }
 
     @NonNull
@@ -97,11 +107,50 @@ public class EventsAdapter extends RecyclerView.Adapter<EventsAdapter.MyViewHold
             e.printStackTrace();
         }
         holder.startDate.setText("Start at: " + startEvent);
+
         Glide.with(context)
                 .load((eventsModel.getImage().equals("null")) ? R.drawable.bau : eventsModel.getImage())
                 .into(holder.image);
 
         long diffInMillis = getDifferenceDT(getCurrentTime(), eventsModel.getStart_date());
+
+        SimpleDateFormat forma = new SimpleDateFormat("yyyy-MM-dd HH:mm");
+        Date date = null;
+        try {
+            date = forma.parse(eventsModel.getStart_date());
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        long millis = date.getTime();
+        Log.d("TIME_IN_MILLI", "onBindViewHolder: " + millis);
+        long alarmTimeBD = millis - DAY_MILLIS;
+        long alarmTimeBH = millis - HOUR_MILLIS;
+        long alarmTimeN = millis;
+
+        SharedPreferences.Editor editor = eventNS.edit();
+
+        boolean isSetForDay = eventNS.getBoolean(eventsModel.getId() + "d", false);
+        boolean isSetForHour = eventNS.getBoolean(eventsModel.getId() + "h", false);
+        boolean isSetForNow = eventNS.getBoolean(eventsModel.getId() + "", false);
+
+        if (diffInMillis >= DAY_MILLIS && !isSetForDay) {
+            scheduleNotification(getNotification(eventsModel.getTitle(), "d"), alarmTimeBD);
+            Log.d("BACKGROUND_SERVICE", "day");
+            editor.putBoolean(eventsModel.getId() + "d", true);
+            editor.commit();
+        }
+        if (diffInMillis >= HOUR_MILLIS && !isSetForHour) {
+            scheduleNotification(getNotification(eventsModel.getTitle(), "h"), alarmTimeBH);
+            Log.d("BACKGROUND_SERVICE", "hour");
+            editor.putBoolean(eventsModel.getId() + "h", true);
+            editor.commit();
+        }
+        if (diffInMillis >= 0 && !isSetForNow) {
+            scheduleNotification(getNotification(eventsModel.getTitle(), "n"), alarmTimeN);
+            Log.d("BACKGROUND_SERVICE", "now");
+            editor.putBoolean(eventsModel.getId() + "", true);
+            editor.commit();
+        }
 
         CountDownTimer countDownTimer = new CountDownTimer(diffInMillis, 1000) {
             @Override
@@ -208,7 +257,7 @@ public class EventsAdapter extends RecyclerView.Adapter<EventsAdapter.MyViewHold
             }
             long seconds = diff.getSeconds();
             if (seconds != 0) {
-                Log.d(TAG, "" + seconds + "s ");
+                Log.d(TAG, "" + (seconds+22) + "s ");
                 difference = seconds + "s ago";
                 return difference;
             }
@@ -221,5 +270,54 @@ public class EventsAdapter extends RecyclerView.Adapter<EventsAdapter.MyViewHold
         Duration diff = Duration.between(LocalDateTime.parse(date1, formatter),
                 LocalDateTime.parse(date2, formatter));
         return diff.toMillis();
+    }
+
+    private void scheduleNotification(Notification notification, long delay) {
+        Intent notificationIntent = new Intent(context, EventBroadcastReceiver.class);
+        int id = new Random(System.currentTimeMillis()).nextInt(1000);
+        notificationIntent.putExtra(EventBroadcastReceiver.NOTIFICATION_ID, id);
+        notificationIntent.putExtra(EventBroadcastReceiver.NOTIFICATION, notification);
+        PendingIntent pendingIntent = PendingIntent.getBroadcast(context, id, notificationIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+        AlarmManager alarmManager = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
+        assert alarmManager != null;
+        alarmManager.set(AlarmManager.RTC_WAKEUP, delay, pendingIntent);
+    }
+
+    @SuppressLint("WrongConstant")
+    private Notification getNotification(String title, String period) {
+        String lng = lang.getString("language", "en");
+        boolean isAR = lng.equals("ar");
+
+        String content = "";
+
+        if (isAR) {
+            if (period.equals("d")) {
+                content = "متبقي يوم واحد على بداية الحدث.";
+            } else if (period.equals("h")) {
+                content = "متبقي ساعة واحدة على بداية الحدث.";
+            } else {
+                content = "بدء الحدث.";
+            }
+        } else {
+            if (period.equals("d")) {
+                content = "One day left until the event begins.";
+            } else if (period.equals("h")) {
+                content = "One hour left until the event begins.";
+            } else {
+                content = "Event has started";
+            }
+        }
+        Bitmap icon = BitmapFactory.decodeResource(Resources.getSystem(), R.mipmap.ic_launcher_round);
+        Uri sound = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(context, default_notification_channel_id);
+        builder.setContentTitle(title);
+        builder.setContentText(content);
+        builder.setSmallIcon(R.drawable.bau);
+        builder.setLargeIcon(icon);
+        builder.setSound(sound);
+        builder.setBadgeIconType(R.mipmap.ic_launcher_round);
+        builder.setAutoCancel(true);
+        builder.setChannelId(NOTIFICATION_CHANNEL_ID);
+        return builder.build();
     }
 }
